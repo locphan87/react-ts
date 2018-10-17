66fd55a7aca7992486bbfb16c0de7887880bda0e
Loc Phan <phanhoangloc@gmail.com>
2018-10-17 18:15:29 +0700

    chore: adjust lint configs

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -2,9 +2,11 @@ import I18n from 'i18n-js'
 import en from './locales/en'
 import vi from './locales/vi'
 
+const defaultLocale = 'en'
+
 I18n.fallbacks = true
-I18n.defaultLocale = 'en'
-I18n.locale = 'en'
+I18n.defaultLocale = defaultLocale
+I18n.locale = defaultLocale
 // choose a different default separator
 // so it's allowed to use dots in i18n keys
 I18n.defaultSeparator = '/'

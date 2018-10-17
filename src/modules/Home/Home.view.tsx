a6b5c9351a8909197156f05970a4f310f17599d3
Walter Ngo <codesigngo@gmail.com>
2018-10-17 11:43:36 +0700

    feat: add i18n-js

--- src/modules/Home/Home.view.tsx
+++ src/modules/Home/Home.view.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import { Page } from '../../components/UI'
+import i18n from '../../i18n'
 import { Container } from './Home.view.style'
 
 const HomeView = () => (
   <Page>
-    <Container>Home</Container>
+    <Container>{i18n.t('hello')}</Container>
   </Page>
 )
 

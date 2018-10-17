604fed8fb4f0c45ded8d05499040ed4937f25363
Loc Phan <phanhoangloc@gmail.com>
2018-10-17 18:08:48 +0700

    chore: add tailwind.macro

--- src/components/UI/View.component.tsx
+++ src/components/UI/View.component.tsx
@@ -1,8 +1,10 @@
 import styled from 'react-emotion'
+// import tw from 'tailwind.macro'
 
 const Page = styled.div({
   display: 'flex',
   justifyContent: 'center'
+  // ...tw`font-mono text-red`
 })
 
 export { Page }

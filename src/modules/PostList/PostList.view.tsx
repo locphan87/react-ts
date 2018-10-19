15ffc07201e95e8ec2b552dd10d67c602875fca6
Loc Phan <phanhoangloc@gmail.com>
2018-10-19 12:09:14 +0700

    feat: add style for PostList

--- src/modules/PostList/PostList.view.tsx
+++ src/modules/PostList/PostList.view.tsx
@@ -1,5 +1,6 @@
 import React, { SFC } from 'react'
 import { Page } from '../../components/UI'
+import { Container } from '../Home/Home.view.style'
 
 interface IPost {
   id: string
@@ -11,13 +12,15 @@ interface IPost {
 interface IProps {
   posts: IPost[]
 }
+
 const PageList: SFC<IProps> = ({ posts = [] }) => (
-  <Page>
-    <div className="flex-col">
+  <Page className="flex-col items-center">
+    <Container>Post List</Container>
+    <div className="flex-col pb-8">
       {posts.map(post => {
         return (
           <div key={post.id} className="flex flex-row">
-            <p>
+            <p className="text-white">
               Id: {post.id} - {post.title} - {post.body} - Author: {post.author}
             </p>
           </div>
